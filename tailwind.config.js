41294c2bbc68cbe45c0ee1a6fea2720963c5542a
Nausheen-UX <imnausheen123@gmail.com>
2026-06-19 18:44:30 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        display: ['"Bebas Neue"', 'sans-serif'],
-        serif:   ['"DM Serif Display"', 'serif'],
-        mono:    ['"Space Mono"', 'monospace'],
-      },
-    },
-  },
+  safelist: [
+    { pattern: /^top-\[/ },
+    { pattern: /^bottom-\[/ },
+    { pattern: /^left-\[/ },
+    { pattern: /^right-\[/ },
+    { pattern: /^text-\[/ },
+    { pattern: /^animate-\[/ },
+    'dots-on',
+  ],
+  theme: { extend: {} },
   plugins: [],
 }
